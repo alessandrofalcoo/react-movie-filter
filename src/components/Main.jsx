f0b6d2300ecdf9951b20af6afbfd041e6ac39644
alessandrofalcoo <alefalco.1003@gmail.com>
2025-03-27 15:44:19 +0100

    feat: implement genre filtering in movie list and update search input

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,16 @@
-import { useState } from 'react'
-import movies from "../data/movieslist"
+import { useEffect, useState } from 'react'
+import Movies from "../data/movieslist"
 
 export default function Main() {
-    const [movie, setMovie] = useState(movies)
+    const [movies, setMovies] = useState(Movies)
+    const [filteredMovie, setFilteredMovie] = useState(Movies)
+    const [selectedGenre, setSelectedGenre] = useState('')
+
+    useEffect(() => {
+        console.log('Hello World');
+        setFilteredMovie(movies.filter(movie => movie.genre.toLowerCase().includes(selectedGenre.toLowerCase())))
+    }, [movies, selectedGenre])
+
     return (
         <div className="container">
             <div className="mb-3">
@@ -12,12 +20,15 @@ export default function Main() {
                     name=""
                     id=""
                     aria-describedby="helpId"
-                    placeholder="Search a movie"
+                    placeholder="Search a genre..."
+                    value={selectedGenre}
+                    onChange={(e) => setSelectedGenre(e.target.value)
+                    }
                 />
 
             </div>
             <ul className="list-group">
-                {movie.map((movie, index) => (
+                {filteredMovie.map((movie, index) => (
                     <li key={index} className="list-group-item d-flex justify-content-between align-items-center">{movie.title}
                     </li>
                 ))}
