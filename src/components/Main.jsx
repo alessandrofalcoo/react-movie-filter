7a9112c7a1b5a049cf1e639ca2594faaafcb4831
alessandrofalcoo <alefalco.1003@gmail.com>
2025-03-27 16:42:10 +0100

    feat: update movie addition form to include genre selection and input validation

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -19,23 +19,25 @@ export default function Main() {
 
     const handleAddMovie = (e) => {
         e.preventDefault()
+        if (newMovie === '') {
+            alert('Devi inserire un film')
+        }
+        console.log(setNewMovie(...movies))
+
+        setNewMovie('')
     }
 
     return (
         <div className="container">
             <div className="mb-3">
-                <input
-                    type="text"
-                    className="form-control"
-                    name=""
-                    id=""
-                    aria-describedby="helpId"
-                    placeholder="Search by genre..."
-                    value={selectedGenre}
-                    onChange={(e) => setSelectedGenre(e.target.value)
-                    }
-                />
+                <select className="my-3 p-1" name="genre" id="genre" value={selectedGenre} onChange={(e) => setSelectedGenre(e.target.value)}>
+                    <option value="">Select a genre</option>
+                    <option value="Fantascienza">Fantascienza</option>
+                    <option value="Thriller">Thriller</option>
+                    <option value="Romantico">Romantico</option>
+                    <option value="Azione">Azione</option>
 
+                </select>
             </div>
             <div className="mb-3">
                 <input
@@ -52,11 +54,12 @@ export default function Main() {
 
             <ul className="list-group">
                 {filteredMovie.map((movie, index) => (
-                    <li key={index} className="list-group-item d-flex justify-content-between align-items-center">{movie.title}
+                    <li key={index} className="list-group-item d-flex justify-content-between align-items-center">
+                        {movie.title}
                     </li>
                 ))}
             </ul>
-            <form>
+            <form onSubmit={handleAddMovie}>
                 <div className="mb-3">
                     <label htmlFor="" className="form-label">Name</label>
                     <input
@@ -66,13 +69,15 @@ export default function Main() {
                         id=""
                         aria-describedby="helpId"
                         placeholder="Add a new movie..."
+                        value={newMovie}
+                        onChange={(e) => setNewMovie(e.target.value)}
                     />
                     <small id="helpId" className="form-text text-muted">Help text</small>
                 </div>
 
             </form>
 
-            <button type="submit" className='btn btn-primary my-3'>Send</button>
+            <button onClick={handleAddMovie} type="submit" className='btn btn-primary my-3'>Send</button>
         </div>
 
     )
